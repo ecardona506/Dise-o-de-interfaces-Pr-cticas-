31dfee992eb8f2da5d7357a56ba7ee0e69cc0721
ecardona506 <ecardona506@javerianacali.edu.co>
2018-08-31 10:09:41 -0500

    Add files via upload

---
+++ Ej1.js
@@ -0,0 +1,16 @@
+function calcular_edad()
+{
+	let edad = document.getElementById('edad').value;
+	var edad_dias = edad * 365;
+	return edad_dias;
+}
+
+function verificar_par()
+{
+	var par = document.getElementById('par').value;
+}
+
+function verificar_par()
+{
+	var impar = document.getElementById('impar').value;
+}
---
+++ control_Ej.js
@@ -0,0 +1,16 @@
+function edad()
+{
+	var edad_dias = calcular_edad()
+	console.log(edad_dias);
+}
+
+function par()
+{
+	var num_par = calcular_edad()
+	console.log(num_par);
+}
+
+function impar()
+{
+	
+}
